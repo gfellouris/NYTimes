39a263887e9de881fd3ec33ea90e15c00495bb4b
geofell <geofell22@gmail.com>
2019-07-09 14:34:59 -0400

    First upload of nytimes code base

---
+++ Phase_III.m
@@ -0,0 +1,13 @@
+* **Instructions:**
+
+  * UI Team:
+
+    * Continue polishing the display of content in the HTML.
+
+    * Consider adding styling or other jQuery tricks.
+
+    * Consider using Bootswatch and/or Font Awesome to add more visual appeal
+
+  * Data Team:
+
+    * Put in a hard-effort to deal with bugs. How can you handle missing fields?
